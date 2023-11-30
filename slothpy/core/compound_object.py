3317fd37ab3d7b27776dfb4179559eb707d9f4ef
Mikolaj Zychowicz <mikolaj.zychowicz@uj.edu.pl>
2023-11-30 19:22:02 +0100

    test

--- slothpy/core/compound_object.py
+++ slothpy/core/compound_object.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# Test
+
 from os import path
 from functools import partial
 from typing import Tuple, Union

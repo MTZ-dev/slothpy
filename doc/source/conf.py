7c6892b08bbb3e9be30d2b812c0d49e08af8b574
Mikolaj Zychowicz <mikolaj.zychowicz@uj.edu.pl>
2024-07-18 07:08:38 +0200

    test of spherical contirbution in free energy

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "SlothPy"
 copyright = "2023, Mikołaj Tadeusz Żychowicz"
 author = "Mikołaj Tadeusz Żychowicz"
-release = "0.2.0"
+release = "0.2.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration

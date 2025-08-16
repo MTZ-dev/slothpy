bdff461e8af3b1b86eb60e1b7d093833133709f1
Mikolaj Zychowicz <mikolaj.zychowicz@uj.edu.pl>
2025-08-16 18:53:05 +0200

    parameter checkpoint

--- relacs_markov.py
+++ relacs_markov.py
@@ -1150,7 +1150,7 @@ def build_matrices(hessian: np.ndarray, masses_inv_sqrt: np.ndarray, dof_array: 
             fwhm_j = gamma_fwhm[t_index] / modes_high / (2 / np.expm1(0.5 * modes_high * beta[0]) + 1) * wb * (2 / np.expm1(0.5 * wb * beta[t_index]) + 1) # * weight
             if not q_0:
                 fwhm_j[:3] = gamma_fwhm[t_index] / modes_high / (2 / np.expm1(0.5 * modes_high * beta[0]) + 1) * max_freq_acoustic * (2 / np.expm1(0.5 * max_freq_acoustic * beta[t_index]) + 1) / max_freq_acoustic**2 / (1/beta[0]/KB)**3 * wb[:3]**2 * (1/beta[t_index]/KB)**3 # * weight
-            cutoff_j = np.minimum(fwhm_j * 50, np.abs(wb + 1.01 * w_n_qtm_max)) 
+            cutoff_j = np.minimum(fwhm_j * 45, np.abs(wb + 1.01 * w_n_qtm_max)) 
             # if thread_id == 0:
             #     print(max_freq_acoustic, 1/beta[t_index]/KB, wb, fwhm_j, cutoff_j)
             # if thread_id == 0:
@@ -1181,8 +1181,8 @@ def build_matrices(hessian: np.ndarray, masses_inv_sqrt: np.ndarray, dof_array: 
 if __name__ == "__main__":
 
     # ── USER-CONFIGURABLE SWEEP LISTS & PARAMETERS ──────────────────────────
-    npoints_list    = [41] # 3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,71,77,81,85,91,101,111,121,131,141,151,161,181,201
-    gamma_fwhm_list = [[0.3]*13]# [[0.5,0.52,0.54,0.56,0.58,0.60,0.62,0.64,0.66,0.68,0.7]]          # FWHM in cm-1
+    npoints_list    = [51] # 3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,71,77,81,85,91,101,111,121,131,141,151,161,181,201
+    gamma_fwhm_list = [[0.38]*13]# [[0.5,0.52,0.54,0.56,0.58,0.60,0.62,0.64,0.66,0.68,0.7]]          # FWHM in cm-1
     T_list          = [1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,3.0] # [4,10,12,15,20,25,30,35,40] # [1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,3.0] # [1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,3.0] # 2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9
     B_list          = [0.1]  # 0.05,0.1,0.2,0.3        # Tesla 0.001,0.002,0.003,0.004,
     states_number   = 6                   # electronic sub-space size

grid normalization fix

ab_relACs/run_ab_relACs.py
@@ -80,7 +80,7 @@ def run_relacs(cfg: AppConfig):
             energies_total *= H_CM_1
 
             for n_points_index, n_points in enumerate(n_points_array):
-                grid, weights = multigrid_aniso(recip_axes, n_points, cfg.relacs.q_ranges)
+                grid, weights, n_k = multigrid_aniso(recip_axes, n_points, cfg.relacs.q_ranges)
                 for fwhm_index, fwhm in enumerate(fwhm_array):
                     if cfg.hessian.dos:
                         weights_int = int_vector_proportional_to_weights(weights)
@@ -99,7 +99,7 @@ def run_relacs(cfg: AppConfig):
                                             temperatures, hessian, masses_inv_sqrt, dof_array, grid,
                                             weights, gamma_fwhm, chi_T, chi_S, cutoff_mult, degeneracy_tolerance,
                                             states_number, modes_low, modes_high, threads, kind, direct,
-                                            run_KR, run_PSI, run_rho0, run_R21, run_R41)
+                                            run_KR, run_PSI, run_rho0, run_R21, run_R41, n_k)
                     sus_T_p = np.abs(sus_T.real) + 1j*np.abs(sus_T.imag)
                     sus_orient_H_T[:,n_points_index,fwhm_index,orientation_index,field_index,:,:] = sus_T_p * orientations_weights[orientation_index]
                     tau_R21_orient_H_T[n_points_index,fwhm_index,orientation_index,field_index,:] = relax_time_R21_T

ab_relACs/susceptibility_relax.py
@@ -346,7 +346,7 @@ def gaussian(dE: float, lw: float, cutoff: float) -> float:
     return prefactor * np.exp(exponent)
 
 @njit(nogil=True, cache=True, fastmath=True, inline="never")
-def add_R41(out: np.ndarray, w_n: np.ndarray, V1: np.ndarray, V2: np.ndarray, n1: float, n2: float, f1: float, f2: float, lw1: float, lw2: float, ind: int, cutoff: float, A: float, B: float, weight: float, sec_tol: float = 1e-6) -> np.ndarray:
+def add_R41(out: np.ndarray, w_n: np.ndarray, V1: np.ndarray, V2: np.ndarray, n1: float, n2: float, f1: float, f2: float, lw1: float, lw2: float, ind: int, cutoff: float, weight: float, sec_tol: float = 1e-6) -> np.ndarray:
     N = w_n.shape[0]
     prefc = pi * pi / H * weight
     lw_total = lw1
@@ -772,24 +772,25 @@ def build_matrices(
     run_rho0: types.Literal,
     run_R21: types.Literal,
     run_R41: types.Literal,
+    n_k: int,
     ):
     raise NotImplementedError
 
 @overload(build_matrices, nogil=True, fastmath=True, cache=True, inline="never", prefer_literal=True)
 def ov_build_matrices(hessian, masses_inv_sqrt, dof_array, H_grad, grid, weights, gamma_fwhm, beta, w_n, M_KR, M_PSI, A_e, rho_vec_init, M_rho0_trace, rho_mat,
-                      R21, R41, cutoff_mult, degeneracy_tolerance, modes_low, modes_high, kind, direct, run_KR, run_PSI, run_rho0, run_R21, run_R41):
+                      R21, R41, cutoff_mult, degeneracy_tolerance, modes_low, modes_high, kind, direct, run_KR, run_PSI, run_rho0, run_R21, run_R41, n_k):
     if isinstance(run_R41, types.Literal):
         if run_R41.literal_value == 0:
             def impl(hessian, masses_inv_sqrt, dof_array, H_grad, grid, weights, gamma_fwhm, beta, w_n, M_KR, M_PSI, A_e, rho_vec_init, M_rho0_trace, rho_mat,
-                     R21, R41, cutoff_mult, degeneracy_tolerance, modes_low, modes_high, kind, direct, run_KR, run_PSI, run_rho0, run_R21, run_R41):
+                     R21, R41, cutoff_mult, degeneracy_tolerance, modes_low, modes_high, kind, direct, run_KR, run_PSI, run_rho0, run_R21, run_R41, n_k):
                 return build_matrices_no_R41(hessian, masses_inv_sqrt, dof_array, H_grad, grid, weights, gamma_fwhm, beta, w_n, M_KR, M_PSI, A_e, rho_vec_init, M_rho0_trace, rho_mat,
-                                             R21, cutoff_mult, degeneracy_tolerance, modes_low, modes_high, kind, direct, run_KR, run_PSI, run_rho0, run_R21)
+                                             R21, cutoff_mult, degeneracy_tolerance, modes_low, modes_high, kind, direct, run_KR, run_PSI, run_rho0, run_R21, n_k)
             return impl
         elif run_R41.literal_value == 1:
             def impl(hessian, masses_inv_sqrt, dof_array, H_grad, grid, weights, gamma_fwhm, beta, w_n, M_KR, M_PSI, A_e, rho_vec_init, M_rho0_trace, rho_mat,
-                     R21, R41, cutoff_mult, degeneracy_tolerance, modes_low, modes_high, kind, direct, run_KR, run_PSI, run_rho0, run_R21, run_R41):
+                     R21, R41, cutoff_mult, degeneracy_tolerance, modes_low, modes_high, kind, direct, run_KR, run_PSI, run_rho0, run_R21, run_R41, n_k):
                 return build_matrices_R41(hessian, masses_inv_sqrt, dof_array, H_grad, grid, weights, gamma_fwhm, beta, w_n, M_KR, M_PSI, A_e, rho_vec_init, M_rho0_trace, rho_mat,
-                                          R21, R41, cutoff_mult, degeneracy_tolerance, modes_low, modes_high, kind, direct, run_KR, run_PSI, run_rho0, run_R21)
+                                          R21, R41, cutoff_mult, degeneracy_tolerance, modes_low, modes_high, kind, direct, run_KR, run_PSI, run_rho0, run_R21, n_k)
             return impl
 
 @njit(nogil=True, fastmath=True, parallel=True, inline="never")
@@ -820,8 +821,9 @@ def build_matrices_no_R41(
     run_PSI: types.Literal,
     run_rho0: types.Literal,
     run_R21: types.Literal,
+    n_k: int,
     ):
-    n_k_inv = 1.0 / np.sqrt(grid.shape[0])
+    n_k_inv = 1.0 / np.sqrt(n_k)
     masses_inv_sqrt_outer = np.outer(masses_inv_sqrt, masses_inv_sqrt)
     gamma = np.asarray([0.0, 0.0, 0.0])
     freq0, modes0 = frequencies_eigenvectors(_build_dynamical_matrix(hessian, masses_inv_sqrt_outer, gamma))
@@ -847,6 +849,8 @@ def build_matrices_no_R41(
         mask = (freq >= modes_low) & (freq <= modes_high)
         idx  = np.where(mask)[0]
         wb, modes = np.ascontiguousarray(freq[idx]), np.ascontiguousarray(modes[:,idx])
+        if wb.shape[0] == 0:
+            continue
         Yb = np.zeros((wb.size, H_grad.shape[1], H_grad.shape[2]), dtype=np.complex128)
         get_Y_q(Yb, H_grad, modes, q, dof_array, masses_inv_sqrt, n_k_inv, wb)
         Yb_table = build_Y_table_j(Yb)
@@ -903,8 +907,9 @@ def build_matrices_R41(
     run_PSI: types.Literal,
     run_rho0: types.Literal,
     run_R21: types.Literal,
+    n_k: int,
     ):
-    n_k_inv = 1.0 / np.sqrt(grid.shape[0])
+    n_k_inv = 1.0 / np.sqrt(n_k)
     masses_inv_sqrt_outer = np.outer(masses_inv_sqrt, masses_inv_sqrt)
     gamma = np.asarray([0.0, 0.0, 0.0])
     freq0, modes0 = frequencies_eigenvectors(_build_dynamical_matrix(hessian, masses_inv_sqrt_outer, gamma))
@@ -923,6 +928,7 @@ def build_matrices_R41(
 
     Yb_array = np.zeros((threads_number, max_grid_size, H_grad.shape[1], H_grad.shape[2]), np.complex128)
     wb_array = np.zeros((threads_number, max_grid_size), np.float64)
+    wq_array = np.zeros((threads_number, max_grid_size), np.float64)
     raman_counter = np.zeros(threads_number, dtype=np.int64)
     raman_counter_wb = np.zeros(threads_number, dtype=np.int64)
     raman_counter_2wb = np.zeros(threads_number, dtype=np.int64)
@@ -941,6 +947,8 @@ def build_matrices_R41(
         mask = (freq >= modes_low) & (freq <= modes_high)
         idx  = np.where(mask)[0]
         wb, modes = np.ascontiguousarray(freq[idx]), np.ascontiguousarray(modes[:,idx])
+        if wb.shape[0] == 0:
+            continue
         Yb = np.zeros((wb.size, H_grad.shape[1], H_grad.shape[2]), dtype=np.complex128)
         get_Y_q(Yb, H_grad, modes, q, dof_array, masses_inv_sqrt, n_k_inv, wb)
         Yb_table = build_Y_table_j(Yb)
@@ -955,6 +963,19 @@ def build_matrices_R41(
         if not q_0:
             Yb_array[thread_id,raman_counter_wb[thread_id]:raman_counter_2wb[thread_id]] = np.conjugate(np.transpose(Yb, (0,2,1)))
             wb_array[thread_id,raman_counter_wb[thread_id]:raman_counter_2wb[thread_id]] = wb
+        
+        # Store the BZ weight for each phonon entry
+        s = raman_counter[thread_id]
+        m = raman_counter_wb[thread_id]
+        e = raman_counter_2wb[thread_id]
+
+        for jj in range(s, m):
+            wq_array[thread_id, jj] = weight
+
+        if not q_0:
+            for jj in range(m, e):
+                wq_array[thread_id, jj] = weight
+
         raman_counter[thread_id] = raman_counter_2wb[thread_id]
 
         for t_index in range(beta.shape[0]):
@@ -979,9 +1000,9 @@ def build_matrices_R41(
 
     wb_array = wb_array.reshape((reshape_size))
     Yb_array = Yb_array.reshape((reshape_size, H_grad.shape[1], H_grad.shape[2]))
+    wq_array = wq_array.reshape((reshape_size))
     N_pairs = wb_array.size * (wb_array.size + 1) // 2
     cutoff_raman = gamma_fwhm * cutoff_mult
-    raman_weight = 1.0
     for t_index_raman in range(beta.shape[0]):
         bose_raman = bose_occ(wb_array, beta[t_index_raman])
         for p in prange(N_pairs):
@@ -991,10 +1012,9 @@ def build_matrices_R41(
             l = p - k*(k + 1)//2
             if wb_array[k] == 0.0 or wb_array[l] == 0.0:
                 continue
-            A = 1 if k!=l else 0.25
-            B = 1 if k!=l else 0.5
+            raman_weight = wq_array[k] * wq_array[l]
             add_R41(R_41_t, w_n, Yb_array[k], Yb_array[l], bose_raman[k], bose_raman[l], wb_array[k],
-                    wb_array[l], gamma_fwhm, gamma_fwhm, thread_id, cutoff_raman, A, B, raman_weight,
+                    wb_array[l], gamma_fwhm, gamma_fwhm, thread_id, cutoff_raman, raman_weight,
                     sec_tol=degeneracy_tolerance)
 
 @njit(nogil=True, fastmath=True, cache=True, inline="never")
@@ -1040,6 +1060,7 @@ def susceptibility_relax_time(
     run_rho0: int,
     run_R21: int,
     run_R41: int,
+    n_k: int,
 ):  
     kind_lit = literally(kind)
     direct_lit = literally(direct)
@@ -1096,7 +1117,7 @@ def susceptibility_relax_time(
                     gamma_fwhm, beta, w_n, M_KR, M_PSI, A_e, rho_vec_init, M_rho0_trace,
                     rho_mat, R21, R41, cutoff_mult, degeneracy_tolerance, modes_low,
                     modes_high, kind_lit, direct_lit, run_KR_lit, run_PSI_lit, run_rho0_lit,
-                    run_R21_lit, run_R41_lit)
+                    run_R21_lit, run_R41_lit, n_k)
 
     M_KR = np.sum(M_KR, axis=0)
     M_PSI = np.sum(M_PSI, axis=0)

ab_relACs/utils.py
@@ -157,6 +157,7 @@ def half_bz_grid_aniso(
     *,
     endpoint: bool = True,
     tol: float = 1e-12,
+    return_n_k: bool = False,
 ) -> np.ndarray:
     """
     Anisotropic half-BZ mesh (unique reps of {+q,−q}) inside an L∞ shell:
@@ -205,6 +206,13 @@ def half_bz_grid_aniso(
     q_unique = full[keep]
 
     idx = np.lexsort(q_unique.T[::-1])
+
+    if return_n_k:
+        n_k = 1
+        for n_k_s in n_axis:
+            n_k *= n_k_s
+        return q_unique[idx], n_k
+
     return q_unique[idx]
 
 def _set_equal_3d(ax, X, Y, Z):
@@ -235,7 +243,7 @@ def multigrid_aniso(
     alpha: float = 0.9,
 ) -> np.ndarray:
     if n_ref == 1:
-        return np.asarray([[0,0,0]], dtype=np.float64), np.asarray([1], dtype=np.float64)
+        return np.asarray([[0,0,0]], dtype=np.float64), np.asarray([1], dtype=np.float64), 1
 
     grids_list = []
     weights_list = []
@@ -246,9 +254,12 @@ def multigrid_aniso(
         start_q = q_ranges[i_q]
         end_q   = q_ranges[i_q-1]
 
-        aniso_grid = half_bz_grid_aniso(
-            b_len, n_ref, start_q, end_q, endpoint=endpoint, tol=tol
-        )
+        if i_q == len(q_ranges) - 1:
+            aniso_grid, n_k = half_bz_grid_aniso(
+            b_len, n_ref, start_q, end_q, endpoint=endpoint, tol=tol, return_n_k=True)
+        else:
+            aniso_grid = half_bz_grid_aniso(
+                b_len, n_ref, start_q, end_q, endpoint=endpoint, tol=tol)
 
         # Half-shell volume in L∞ norm
         vol_full = (2.0*start_q)**3 - (2.0*end_q)**3
@@ -297,7 +308,7 @@ def multigrid_aniso(
             ax.figure.canvas.draw_idle()
             plt.show()
 
-    return grid, weights
+    return grid, weights, n_k
 
 
 def make_npoints_fwhm_filename(filepath: Union[str, Path], npoints: int, fwhm: float, additional: str = "") -> str: